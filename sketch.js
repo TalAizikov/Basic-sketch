ace5ae4735cb58de7ad4374f6915325ac6acce9f
TalAizikov <31697771+TalAizikov@users.noreply.github.com>
2017-09-13 11:39:49 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,14 +8,3 @@ function draw() {
     fill(10,100,200);
     ellipse(mouseX, mouseY, 50, 50);
 }
-var randomShape = Math.floor(Math.random() * 2) + 1;
-if (randomShape === 1) {
-    function draw() {
-        ellipse(50,50,50,50);
-    }
-}
-else  {
-    function draw() {
-        rect(50,50,50,50);
-    }
-}
